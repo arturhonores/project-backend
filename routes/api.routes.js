8627af3223d9613f8206da55ce5f322d5b8c8365
arturhonores <luis.arturhonores@gmail.com>
2023-05-15 15:03:58 +0200

    first commit branch arturo

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,62 +1,63 @@
 const router = require("express").Router();
-const axios = require("axios");
+const axios = require('axios');
 
-// let nextCursor = null;
+let cursor = ''; // Variable para almacenar el valor del cursor
 
-// const options = {
-//     method: 'GET',
-//     url: 'https://streaming-availability.p.rapidapi.com/v2/search/pro',
-//     params: {
-//         country: 'us',
-//         services: 'netflix',
-//         output_language: 'en',
-//         show_type: 'movie',
-//         // genres: '18,80',
-//         genres_relation: 'or',
-//         // keyword: 'zombie',
-//         show_original_language: 'en',
-//         year_min: '2021',
-//         year_max: '2025',
-//         order_by: 'year',
-//         desc: 'true'
-//     },
-//     headers: {
-//         'X-RapidAPI-Key': '008a72a717msh3f3f676623b2669p142376jsnb704aca49cf5',
-//         'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-//     }
-// };
+const options = {
+    method: 'GET',
+    url: 'https://streaming-availability.p.rapidapi.com/v2/search/pro',
+    params: {
+        country: 'es',
+        services: 'prime.subscription',
+        output_language: 'en',
+        show_type: 'movie',
+        // genres: '4',
+        genres_relation: 'or',
+        // keyword: 'zombie',
+        show_original_language: 'en',
+        year_min: '2022',
+        year_max: '2025',
+        order_by: 'year',
+        desc: 'true',
+        cursor: cursor // Incluir el valor del cursor en los parámetros
+    },
+    headers: {
+        'X-RapidAPI-Key': '008a72a717msh3f3f676623b2669p142376jsnb704aca49cf5',
+        'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+};
 
-// function fetchMovies(req, res, next) {
-//     if (nextCursor) {
-//         // Si hay un valor de cursor almacenado, se agrega a los parámetros de la solicitud
-//         options.params.cursor = nextCursor;
-//     }
+router.get("/peliculas-new", (req, res, next) => {
+    console.log("Valor actual del cursor:", cursor);
+    axios.request(options)
+        .then(response => {
+            const movieData = response.data;
+            const hasMore = movieData.hasMore;
+            const nextCursor = movieData.nextCursor;
+            const escapedNextCursor = encodeURIComponent(nextCursor);
 
-//     axios
-//         .request(options)
-//         .then(response => {
-//             res.render("api/new-movie", { movie: response.data });
-//             console.log(response.data);
-
-//             if (response.data.hasMore && nextCursor !== response.data.nextCursor) {
-//                 // Si la respuesta indica que hay más resultados disponibles y el cursor es diferente, se almacena el valor del cursor
-//                 nextCursor = response.data.nextCursor;
-//                 // Se realiza la siguiente solicitud
-//                 fetchMovies(req, res, next);
-//             } else {
-//                 // Si no hay más resultados, se reinicia el valor del cursor a null
-//                 nextCursor = null;
-//                 next();
-//             }
-//         })
-//         .catch(err => {
-//             next(err);
-//         });
-// }
-
-// router.get("/peliculas-new", fetchMovies, (req, res, next) => {
-//     // Aquí puedes realizar cualquier acción adicional después de que se hayan obtenido todas las películas
-//     next();
-// });
+            res.render("api/new-movie", {
+                movie: movieData,
+                hasMore: hasMore,
+                nextCursor: escapedNextCursor
+            });
+            console.log(response.data);
+            if (hasMore) {
+                cursor = nextCursor; // Obtener el valor del cursor para la siguiente solicitud
+                options.params.cursor = cursor; // Actualizar el valor del cursor en los parámetros
+                // Realizar una nueva solicitud con el nuevo cursor
+                axios.request(options)
+                    .then(response => {
+                        // Realizar las operaciones necesarias con la respuesta adicional
+                    })
+                    .catch(err => {
+                        next(err);
+                    });
+            }
+        })
+        .catch(err => {
+            next(err);
+        });
+});
 
 module.exports = router;
