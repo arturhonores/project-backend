8e3602a547a037b4db0033ec77e6c83df0863c06
arturhonores <luis.arturhonores@gmail.com>
2023-05-15 23:56:09 +0200

    buttons previous and next

--- services/stream-api.service.js
+++ services/stream-api.service.js
@@ -9,44 +9,68 @@ class MoviesApiHandler {
                 'X-RapidAPI-Host': process.env.RAPIDAPI_HOST
             }
         });
-        this.cursor = '';
         this.previousCursors = [];
     }
 
-    getMovies(req, res, next) {
-        console.log("Valor actual del cursor:", this.cursor);
-        const options = {
-            method: 'GET',
-            url: '/search/pro',
-            params: {
-                country: 'us',
-                services: 'prime.subscription',
-                output_language: 'en',
-                genres: '80',
-                show_type: 'movie',
-                genres_relation: 'or',
-                show_original_language: 'en',
-                year_min: '2022',
-                year_max: '2025',
-                order_by: 'year',
-                desc: 'true',
-                cursor: this.cursor
-            }
+    createQueryParams(cursor) {
+        return {
+            country: 'es',
+            services: 'prime.subscription',
+            output_language: 'en',
+            genres: '80',
+            show_type: 'movie',
+            genres_relation: 'or',
+            show_original_language: 'en',
+            year_min: '2021',
+            year_max: '2025',
+            order_by: 'year',
+            desc: 'true',
+            cursor: cursor
         };
+    }
 
-        this.axiosApp
-            .request(options)
-            .then(response => {
-                const movieData = response.data;
-                res.render('api/new-movie', {
-                    movie: movieData,
-                });
-                console.log(response.data);
-            })
-            .catch(err => {
-                next(err);
+    updatePreviousCursors(cursor) {
+        if (cursor) {
+            const index = this.previousCursors.indexOf(cursor);
+            if (index !== -1) {
+                this.previousCursors = this.previousCursors.slice(0, index);
+            } else {
+                this.previousCursors.push(cursor);
+            }
+        }
+    }
+
+    async getMovies(req, res, next) {
+        const cursor = req.query.cursor || '';
+
+        this.updatePreviousCursors(cursor);
+
+        console.log("Valor actual del cursor:", cursor);
+
+        try {
+            const options = {
+                method: 'GET',
+                url: '/search/pro',
+                params: this.createQueryParams(cursor)
+            };
+
+            const response = await this.axiosApp.request(options);
+            const movieData = response.data;
+
+            res.render('api/new-movie', {
+                movie: movieData,
+                hasPrevious: this.previousCursors.length > 1,
+                previousCursor: this.previousCursors[this.previousCursors.length - 2],
+                hasMore: movieData.hasMore,
+                nextCursor: movieData.nextCursor
             });
+
+            console.log(response.data);
+        } catch (err) {
+            next(err);
+        }
     }
 }
 
 module.exports = MoviesApiHandler;
+
