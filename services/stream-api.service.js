4d9c19ce65d505e7f01f5bbd9e0e5b74f0061b2a
arturhonores <luis.arturhonores@gmail.com>
2023-05-17 12:12:11 +0200

    await reemplazado por then

--- services/stream-api.service.js
+++ services/stream-api.service.js
@@ -17,7 +17,7 @@ class MoviesApiHandler {
             country: 'es',
             services: services || 'prime.subscription,netflix,disney,hbo',
             output_language: 'en',
-            genres: genres || '1,3,4,5,12',
+            genres: genres || '1, 2, 3, 4, 5, 6, 7, 12, 14, 16, 18, 27, 28, 35, 36, 37, 53, 80, 99, 878, 9648, 10402, 10749, 10751, 10752, 10763, 10764, 10767',
             show_type: 'movie',
             genres_relation: 'or',
             show_original_language: 'en',
@@ -54,89 +54,74 @@ class MoviesApiHandler {
     }
 
 
-    async getMovies(req, res, next) {
+    getMovies(req, res, next) {
         const cursor = req.query.cursor || '';
 
         this.updatePreviousCursors(cursor);
 
         console.log("Valor actual del cursor:", cursor);
 
-        try {
-            const options = {
-                method: 'GET',
-                url: '/search/pro',
-                params: this.createQueryParams(cursor)
-            };
+        const options = {
+            method: 'GET',
+            url: '/search/pro',
+            params: this.createQueryParams(cursor)
+        };
 
-            const response = await this.axiosApp.request(options);
-            const movieData = response.data;
+        this.axiosApp.request(options)
+            .then(response => {
+                const movieData = response.data;
 
-            //para obtener info de streamingInfo
-            const movies = response.data.result.map(movie => {
-                return {
-                    ...movie,
-                    streamingData: this.processStreamingInfo(movie),
-                };
+                const movies = response.data.result.map(movie => {
+                    return {
+                        ...movie,
+                        streamingData: this.processStreamingInfo(movie),
+                    };
+                });
+
+                res.render('api/new-movie', {
+                    movie: {
+                        ...movieData,
+                        result: movies,
+                    },
+                    hasPrevious: this.previousCursors.length > 1,
+                    previousCursor: this.previousCursors[this.previousCursors.length - 2],
+                    hasMore: movieData.hasMore,
+                    nextCursor: movieData.nextCursor
+                });
+
+                console.log(response.data);
+            })
+            .catch(err => {
+                next(err);
             });
-            //
-
-            res.render('api/new-movie', {
-                movie: {
-                    ...movieData,
-                    result: movies,
-                },
-                hasPrevious: this.previousCursors.length > 1,
-                previousCursor: this.previousCursors[this.previousCursors.length - 2],
-                hasMore: movieData.hasMore,
-                nextCursor: movieData.nextCursor
-            });
-
-            console.log(response.data);
-            // saber qué contiene el elemento streamingInfo
-            // const movies = response.data.result;
-
-            // movies.forEach(movie => {
-            //     console.log(movie.streamingInfo); // Para ver la información de streaming de la película
-            // });
-
-            // movies.forEach(movie => {
-            //     for (const country in movie.streamingInfo) {
-            //         console.log(`Información de streaming para el país ${country}:`, movie.streamingInfo[country]);
-            //     }
-            // });
-            // ///////////////////////////////////////////7
-
-
-        } catch (err) {
-            next(err);
-        }
     }
 
-    async getFilteredMovies(req, res, next) {
+    getFilteredMovies(req, res, next) {
         const { cursor, services, genres } = req.body;
         console.log(req.body)
 
-        try {
-            const options = {
-                method: 'GET',
-                url: '/search/pro',
-                params: this.createQueryParams(cursor, services, genres)
-            };
+        const options = {
+            method: 'GET',
+            url: '/search/pro',
+            params: this.createQueryParams(cursor, services, genres)
+        };
 
-            const response = await this.axiosApp.request(options);
-            const movieData = response.data;
+        this.axiosApp.request(options)
+            .then(response => {
+                const movieData = response.data;
 
-            res.render('api/movie-filter', {
-                movie: movieData,
-                hasPrevious: this.previousCursors.length > 1,
-                previousCursor: this.previousCursors[this.previousCursors.length - 2],
-                hasMore: movieData.hasMore,
-                nextCursor: movieData.nextCursor
+                res.render('api/movie-filter', {
+                    movie: movieData,
+                    hasPrevious: this.previousCursors.length > 1,
+                    previousCursor: this.previousCursors[this.previousCursors.length - 2],
+                    hasMore: movieData.hasMore,
+                    nextCursor: movieData.nextCursor
+                });
+
+            })
+            .catch(err => {
+                next(err);
             });
-
-        } catch (err) {
-            next(err);
-        }
     }
 
 }
